ebb6c4ed930214780851fe1c136d0589cd0a0fb3
klar2d2 <koberyan@comcast.net>
2019-09-22 09:34:17 -0700

    commit before upstream

--- src/App.tsx
+++ src/App.tsx
@@ -40,11 +40,12 @@ class App extends Component<AppProps, IAppState> {
     const token = localStorage.getItem("mernToken");
     // If theres a token, try to use it ot get the user info
     if (token) {
+      console.log(token)
       axios.post(CURRENT_USER, {
         headers: {Authorization: `Bearer ${token}` },
       })
       .then((response) => {
-        console.log("app resp", response)
+        console.log("app resp ", response) 
         this.setState({ user: response.data.user });
         this.setState({ current: response.data.user });
       })
